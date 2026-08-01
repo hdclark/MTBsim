694f244af8072d969b5d4dcee7343254df48f4d7
Hal Clark <hdclark@ualberta.ca>
2026-08-01 01:01:48 -0700

    Add the JUnit Platform launcher at runtime

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,5 +61,7 @@ kotlin {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:5.13.4")
+    testImplementation(platform("org.junit:junit-bom:5.13.4"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
